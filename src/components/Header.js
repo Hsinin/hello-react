c2bc7d4e303a7034991ba7b90dd4c258492f4037
xin_wang <wangxin16@asiainfo-sec.com>
2019-02-26 22:22:44 +0800

    Video10
    1. 无状态组件写法，好处
    2. es5 es6 区别

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
-    render(){
-        return (
-            <div className="container">
-              <div className="row">
-                <div className="col-xs-1 col-xs-offset-11">
-                  <div>"Header"</div>
-                </div>
-              </div>
-            </div>
-          );
-    }
+// class Header extends Component {
+//     render(){
+//         return (
+//             <div className="container">
+//               <div className="row">
+//                 <div className="col-xs-1 col-xs-offset-11">
+//                   <div>"Header"</div>
+//                 </div>
+//               </div>
+//             </div>
+//           );
+//     }
   
+// };
+
+//无状态组件写法
+const Header = (prpos) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-1 col-xs-offset-11">
+          <div>"Header"</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
